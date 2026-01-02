7b0451c3df31fd9b9467036d0b6a0043f4947c89
kahqgc <kahqgc@gmail.com>
2026-01-02 13:24:34 -0600

    file structure changes

--- wedding-weekend-itinerary/src/App.jsx
+++ wedding-weekend-itinerary/src/App.jsx
@@ -2,10 +2,10 @@ import "./App.css";
 import { useState } from "react";
 import Header from "./components/Header.jsx";
 import BottomNav from "./components/BottomNav.jsx";
-import ScheduleView from "./components/ScheduleView.jsx";
-import InfoView from "./components/InfoView.jsx";
-import MapView from "./components/MapView.jsx";
-import FaqView from "./components/FaqView.jsx";
+import ScheduleView from "./views/ScheduleView.jsx";
+import InfoView from "./views/InfoView.jsx";
+import MapView from "./views/MapView.jsx";
+import FaqView from "./views/FaqView.jsx";
 
 function App() {
   // VEGAS_WEDDING_MASTER_SCHEDULE_SPA_FRIDAY
